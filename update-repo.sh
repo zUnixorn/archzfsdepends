5e7cf814b08ff599e492cc68b955206b3f8776b5
zUnixorn <77864446+zUnixorn@users.noreply.github.com>
2023-02-12 00:24:21 +0100

    Update update-repo.sh

--- update-repo.sh
+++ update-repo.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 git clone git@github.com:zUnixorn/archzfsdepends.git
-cp archzfsdepends/archzfs_synchronize.py .
 cd archzfsdepends
 git checkout github-pages
 mkdir -p zfsdepends
-python ../archzfs_synchronize.py
+python /archzfs_synchronize.py
 git commit -m "updated repo"
 git push
